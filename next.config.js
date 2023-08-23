20dfbc5259559af306a3db6ab2b8000403039ab4
Muhammad Waqar Ulhaque <99873601+WAQAR1205@users.noreply.github.com>
2023-08-24 01:02:24 +0500

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images:{
+        domains:['universalmotorstorage.blob.core.windows.net']
+    }
+}
+
+module.exports = nextConfig
